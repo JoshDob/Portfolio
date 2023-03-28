c4bf91de03f6835957a6720c947a02b6dd6f1375
JoshDob <joshdobrowner@gmail.com>
2023-03-28 15:40:15 -0700

    1st commit

---
+++ src/components/Display.svelte
@@ -0,0 +1,15 @@
+<div class="outer">
+  <div class="inner" />
+</div>
+
+<style>
+  .outer {
+    height: 100%;
+    padding: 1.25rem;
+  }
+
+  .inner {
+    height: 100%;
+    border: 1px solid red;
+  }
+</style>
---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { projectImages } from "../projectImages.js";
+</script>
+
+<div class="projects">
+  {#each projectImages as projectImage, index}
+    <div
+      class="project-thumbnail"
+      style="background-image: url({projectImage.src});"
+    />
+  {/each}
+</div>
+
+<style>
+  .projects {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 2rem;
+  }
+
+  .project-thumbnail {
+    background-position: center;
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-size: cover;
+    border-radius: 10px;
+    cursor: pointer;
+    height: 150px;
+    transition: box-shadow 0.3s ease-in-out, transform 0.3s ease-in-out;
+    width: 150px;
+  }
+
+  .project-thumbnail:hover {
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
+    transform: scale(1.05);
+  }
+</style>
