798c111fad27febebf21f814a2f2225b3ba705ea
JoshDob <joshdobrowner@gmail.com>
2023-03-28 14:30:40 -0700

    1st Commit

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Background from "./components/Background.svelte";
+  import Sidebar from "./components/Sidebar.svelte";
+  import Display from "./components/Display.svelte";
+</script>
+
+<Background />
+<Sidebar />
+<Display />
