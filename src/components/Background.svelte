5520aafcb5cc1f8a41e19361bb0e3d7ed5bc60a3
JaDobrowner <110553239+JoshDob@users.noreply.github.com>
2023-03-28 14:43:01 -0700

    Add files via upload

---
+++ src/components/Background.svelte
@@ -0,0 +1,35 @@
+<!-- Frame.svelte -->
+<script>
+  // You can add props or logic here if needed
+</script>
+
+<div class="wrap">
+  <div class="half left" />
+  <div class="half right" />
+</div>
+
+<style>
+  .wrap {
+    position: fixed;
+    display: flex;
+    flex-direction: row;
+    width: 100vw;
+    height: 100vh;
+    z-index: -1;
+  }
+
+  .half {
+    flex: 1;
+    height: 100%;
+  }
+
+  .left {
+    background-color: #030708;
+    background-image: linear-gradient(45deg, #64807e, #060808);
+  }
+
+  .right {
+    background-color: #030708;
+    background-image: linear-gradient(135deg, #060808, #64807e);
+  }
+</style>
---
+++ src/projectImages.js
@@ -0,0 +1,11 @@
+
+export const projectImages = [
+  {
+    src: 'https://cdn.midjourney.com/1d848e95-2e0f-4b9c-8df6-45da74f18817/0_1.png',
+    title: 'Project 1',
+    gradient: 'linear-gradient(180deg, rgba(0, 0, 0, 0.5) 0%, rgba(0, 0, 0, 0) 100%)',
+  },
+  { src: 'https://cdn.midjourney.com/62123298-741f-4f79-b6bc-047d2c6c9244/0_2.png',
+    title: 'Project 2',
+    gradient: 'linear-gradient(180deg, rgba(0, 0, 0, 0.5) 0%, rgba(0, 0, 0, 0) 100%)'
+   }]
